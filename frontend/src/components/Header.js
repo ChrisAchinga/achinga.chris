692eb9cc2bf3f0c30853153286bd15542daa3d36
ChrisAchinga <achinga.chris@gmail.com>
2020-11-19 16:34:35 +0300

    components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Chris Dev</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/projects'>
+                <Nav.Link>
+                <i className="fas fa-warehouse"></i> Projects
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/blogs'>
+                <Nav.Link>
+                <i className="fas fa-book-open"></i> Blogs
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
