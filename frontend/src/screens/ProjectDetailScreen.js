5e5514126074e7dfaa8a0f17004c0f31a1fe7555
ChrisAchinga <achinga.chris@gmail.com>
2020-11-26 18:30:49 +0300

    clean

--- frontend/src/screens/ProjectDetailScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// import { Link } from 'react-router-dom'
-// import { Row, Col, Image, Card, Button } from 'react-bootstrap'
-
-const ProjectDetailScreen = () => {
-  return <>Project Details</>
-}
-
-export default ProjectDetailScreen
