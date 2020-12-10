b3b2067659d7dec557fb6b419988a9830c52f200
ChrisAchinga <achinga.chris@gmail.com>
2020-12-10 15:47:07 +0300

    gh-pages deploy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1>Chris Achinga</h1>
+        <p>Website under maintenance</p>
+        <p>Email Me: achinga.chris@gmail.com</p>
+      </Container>
+    </>
+  )
+}
+
+export default App
