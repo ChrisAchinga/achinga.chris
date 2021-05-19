6412f1bf45b5562fe33c93d75f2ab2654c91c4ca
ChrisAchinga <achinga.chris@gmail.com>
2021-05-19 15:14:05 +0300

    clean

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '@styles/globals.css'
-import '@styles/body-font.css'
-import '@styles/heading-font.css'
-
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
