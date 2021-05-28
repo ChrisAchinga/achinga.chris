c2405ee2352956bacd4a258658cbe395c9f10e03
ChrisAchinga <achinga.chris@gmail.com>
2021-05-28 17:34:22 +0300

    popo

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Chris Achinga</h1>
+
+      <hr />
+      <a href='https://www.linkedin.com/in/chrisachinga/'>LinkedIN</a> <br />
+      <a href='https://github.com/ChrisAchinga'>GitHub</a> <br />
+      <a href='https://twitter.com/achinga_chris'>Twitter</a>
+    </div>
+  )
+}
+
+export default Home
