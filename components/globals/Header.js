34b1b2cffbba7ef13df78633813a26ef57b510ea
ChrisAchinga <achinga.chris@gmail.com>
2021-06-12 23:14:22 +0300

    header and footer init

--- components/globals/Header.js
+++ components/globals/Header.js
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+
 const Header = () => {
-    return (
-        <div>Header</div>
-    )
+  return (
+    <header>
+      <nav>
+        <Link href='/'>home</Link>
+        <Link href='/resume'>resume</Link>
+        <Link href='/blog'>blog</Link>
+        <Link href='/projects'>projects</Link>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
