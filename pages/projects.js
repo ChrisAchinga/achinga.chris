2f023f02fb2ccc65078c49fcd4b07a0a29da7c2c
ChrisAchinga <achinga.chris@gmail.com>
2021-06-12 23:15:26 +0300

    sitemaps api

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import Layout from '@/layout/Layout'
+
+const Projects = () => {
+  return (
+    <Layout
+      pageTitle='Chris Achinga Projects'
+      pageDescription='All ChrisDevCode Projects'
+      pageURL='https://chrisdev.vercel.app/projects'
+    >
+      <h1>My Projects</h1>
+    </Layout>
+  )
+}
+
+export default Projects
