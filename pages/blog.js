382366ade7bbeffebe2983c00621e06681b59b59
ChrisAchinga <achinga.chris@gmail.com>
2021-06-13 19:16:06 +0300

    fetch data from hashnode

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,8 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 import Layout from '@/layout/Layout'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
+  // console.log('posts', posts)
   return (
     <Layout
       pageTitle='Chris Achinga Technical Articles'
@@ -8,8 +10,47 @@ const Blog = () => {
       pageURL='https://chrisdev.vercel.app/blog'
     >
       <h1>My Technical Blogs</h1>
+      {posts.map((post) => {
+        return (
+          <div key={post._id}>
+            <h1>{post.title}</h1>
+            <a href={`https://chrisdevcode.hashnode.dev/${post.slug}`}>Read</a>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://api.hashnode.com/',
+    cache: new InMemoryCache(),
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      query GetPosts {
+        user(username: "chrisdevcode") {
+          publication {
+            posts(page: 0) {
+              _id
+              coverImage
+              slug
+              title
+              brief
+            }
+          }
+        }
+      }
+    `,
+  })
+
+  return {
+    props: {
+      posts: data.user.publication.posts,
+    },
+  }
+}
