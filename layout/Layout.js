43a9e87a17fedddabbcc3a7e7d7b01b8921c452a
ChrisAchinga <achinga.chris@gmail.com>
2021-06-17 00:34:20 +0300

    use scripts

--- layout/Layout.js
+++ layout/Layout.js
@@ -4,6 +4,8 @@ import Seo from '@/globals/Seo'
 import Header from '@/globals/Header'
 import Footer from '@/globals/Footer'
 
+// import jscript from '../public/scripts/scripts.js'
+
 const Layout = ({ children, pageTitle, pageDescription, pageURL }) => {
   return (
     <>
@@ -15,6 +17,24 @@ const Layout = ({ children, pageTitle, pageDescription, pageURL }) => {
       <Header />
       <main>{children}</main>
       <Footer />
+      {/* third party js plugins */}
+      <Script
+        src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
+        strategy='afterInteractive'
+      />
+      <Script
+        src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'
+        strategy='afterInteractive'
+      />
+      <Script
+        src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js'
+        strategy='afterInteractive'
+      />
+      {/* <Script
+        async
+        src='../public/scripts/scripts.js'
+        strategy='afterInteractive'
+      /> */}
     </>
   )
 }
